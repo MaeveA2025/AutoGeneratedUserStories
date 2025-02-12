39d60387217bc8811d20fbf48d6936ab768b21cf
Will Meister <meisterwilliam5@gmail.com>
2025-02-12 09:03:57 -0500

    removed unused section of card

--- backend/user-stories-api/src/app/components/story_card.tsx
+++ backend/user-stories-api/src/app/components/story_card.tsx
@@ -15,16 +15,6 @@ class StoryCard extends React.Component<StoryCardProps> {
                 <div className="card-body">
                     <h5 className="card-title">{story.name}</h5>
                     <p className="card-text">{story.description}</p>
-                    {story.acceptanceCriteria.length > 0 && ( 
-                        <div>
-                            <h6>Acceptance Criteria:</h6>
-                            <ul className="list-unstyled">
-                                {story.acceptanceCriteria.map((criteria, index) => (
-                                    <li key={index}>{criteria}</li>
-                                ))}
-                            </ul>
-                        </div>
-                    )}
                 </div>
             </div>
         );
